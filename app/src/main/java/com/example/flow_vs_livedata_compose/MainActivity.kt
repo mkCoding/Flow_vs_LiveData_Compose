22ab23dc0182f5ee384daf7eb3bca98de9cc821e
Mike Kemakolam <mikekemakolam@MBPM2-HGKM633XMW-KEMAKOLAM.local>
2024-06-22 11:25:34 -0400

    initial commit

---
+++ app/src/main/java/com/example/flow_vs_livedata_compose/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.flow_vs_livedata_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.flow_vs_livedata_compose.ui.theme.Flow_vs_LiveData_ComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Flow_vs_LiveData_ComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Flow_vs_LiveData_ComposeTheme {
+        Greeting("Android")
+    }
+}
